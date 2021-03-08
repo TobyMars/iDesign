cf7d54ecbd8bb72699ca1f2e658544b77af39c7a
TobyMars <hk.mars@yahoo.com>
2021-03-08 13:59:44 +0800

    Create Xcode project for Monalisp Design

---
+++ doc/product/monalisp/Monalisp/Monalisp/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Monalisp
+//
+//  Created by TobyMars on 2021/3/8.
+//  Copyright © 2021 TobyMars. All rights reserved.
+//
+
+#include <stdio.h>
+
+// TODO:
+// Source codes of Monalisp:
+// 1. https://github.com/hk-mars/mona-lisp
+// 2. https://github.com/TobyMars/iVM
+//
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, Monalisp!\n");
+    return 0;
+}
+
